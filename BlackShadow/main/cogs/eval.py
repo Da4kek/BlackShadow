573e518730f4cc365c83d690de0c7e834c83da7f
BuildTools <unconfigured@null.spigotmc.org>
2021-04-17 12:12:04 +0530

    added eval which works

--- BlackShadow/main/cogs/eval.py
+++ BlackShadow/main/cogs/eval.py
@@ -2,26 +2,70 @@ import discord
 from discord.ext import commands
 import contextlib
 import io
-class Eval(commands.Cog):
-    def __init__(self,bot):
+import re
+from discord import Color, Embed
+
+
+class CodeExec(commands.Cog):
+    def __init__(self, bot: commands.Bot):
         self.bot = bot
+        # TODO: Improve this further
+        self.regex = re.compile(r"(\w*)\s*(?:```)(\w*)?([\s\S]*)(?:```$)")
+
+    @property
+    def session(self):
+        return self.bot.http._HTTPClient__session
+
+    async def _run_code(self, *, lang: str, code: str):
+        res = await self.session.post(
+            "https://emkc.org/api/v1/piston/execute",
+            json={"language": lang, "source": code},
+        )
+        return await res.json()
+
     @commands.command()
-    async def eval(self,ctx,*,code):
-        str_obj = io.StringIO()
-        try:
-            with contextlib.redirect_stdout(str_obj):
-                exec(code)
-        except Exception as e:
-            return await ctx.send(f"```{e.__class__.__name__}: {e}```")
-        try:
-            em = discord.Embed(color= discord.Color.light_grey())
-            em.add_field(
-                name=f"Ran your {eval(code)} code", value=f"```{str_obj.getvalue()}```")
-            em.set_footer(icon_url=ctx.author.avatar_url)
-        except:
-            await ctx.send("An error occured")
+    async def run(self, ctx: commands.Context, *, codeblock: str):
+        matches = self.regex.findall(codeblock)
+        if not matches:
+            return await ctx.send(
+                embed=Embed(
+                    title="__Error!:negative_squared_cross_mark:__", description="Try again with **Code blocks**"
+                ,color=Color.red())
+            )
+        lang = matches[0][0] or matches[0][1]
+        if not lang:
+            return await ctx.send(
+                embed=Embed(
+                    title="__Error!:negative_squared_cross_mark:__",
+                    description="Try again with **valid language**",
+                )
+            )
+        code = matches[0][2]
+        result = await self._run_code(lang=lang, code=code)
 
-        await ctx.send(embed = em)
+        await self._send_result(ctx, result)
 
-def setup(bot):
-    bot.add_cog(Eval(bot))
+
+
+    async def _send_result(self, ctx: commands.Context, result: dict):
+        if "message" in result:
+            return await ctx.send(
+                embed=Embed(
+                    title="__Error!:negative_squared_cross_mark:__", description=result["message"], color=Color.red()
+                )
+            )
+        output = result["output"]
+        
+        output = output[:500]
+        shortened = len(output) > 500
+        lines = output.splitlines()
+        shortened = shortened or (len(lines) > 15)
+        output = "\n".join(lines[:15])
+        output += shortened * "\n\n**Short output**"
+        embed = Embed(
+            title=f"**__Output__**", color=Color.green(),description = output or "**No Output**")
+        await ctx.reply(embed=embed)
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(CodeExec(bot))
